48eaaf404eecce77f930504e612521f75834d174
rattanachai-jobmail-gh <rattanachai.jobmail2543@gmail.com>
2026-05-04 22:05:37 +0700

    Fix SSR route refresh handling on Vercel

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,19 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'login',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'add-product',
-    renderMode: RenderMode.Client
-  },
-  {
-    path: 'find-product',
-    renderMode: RenderMode.Client
-  },
-  {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Server
   }
 ];
